ca1aa852519ff11e40e0c441336c1e538fa10ba4
izi-dot <martin.kuciak.2@gmail.com>
2026-01-07 22:34:57 +0100

    uprava api, pridanie descriptionu, uprava databazy

--- src/database.controller.ts
+++ src/database.controller.ts
@@ -1,6 +1,6 @@
 import mysql, { type Pool, type RowDataPacket } from 'mysql2/promise';
 import { config } from './config.js';
-import { Post, type NewPostRequest } from 'domains/post.domain.js';
+import { Post, type NewPostRequest } from './domains/post.domain.js';
 import { v4 as uuidv4 } from 'uuid';
 
 
@@ -28,15 +28,16 @@ export class DatabaseController {
 
   async createPost(request: NewPostRequest): Promise<void> {
     const query = `INSERT INTO post 
-      (id, created_by, user_avatar, pet_name, location, photos) 
-      VALUES (?, ?, ?, ?, ?, ?)`;
+      (id, created_by, user_avatar, pet_name, location, photos, description) 
+      VALUES (?, ?, ?, ?, ?, ?, ?)`;
     const values = [
       uuidv4(),
       request.createdBy,
       request.userAvatar,
       request.petName,
       request.location,
-      request.photos
+      request.photos,
+      request.description
     ];
 
     await this.db.execute(query, values);
